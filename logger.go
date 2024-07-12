dc9a79160806db74a0d55b9ee448b92b89037636
ricardo <ohcanikissu@gmail.com>
2024-07-12 15:19:26 +0800

    feat: 添加普通文本日志，支持高亮

--- logger.go
+++ logger.go
@@ -220,6 +220,98 @@ func NewJSONLogger(opts ...Option) (*zap.Logger, error) {
 	return log, nil
 }
 
+// NewTextLogger return a simple-encoder zap logger,
+func NewTextLogger(opts ...Option) (*zap.Logger, error) {
+	opt := &option{level: DefaultLevel, fields: make(map[string]string)}
+	for _, f := range opts {
+		f(opt)
+	}
+
+	timeLayout := DefaultTimeLayout
+	if opt.timeLayout != "" {
+		timeLayout = opt.timeLayout
+	}
+
+	// similar to zap.NewProductionEncoderConfig()
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger", // used by logger.Named(key); optional; useless
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace", // use by zap.AddStacktrace; optional; useless
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.LowercaseLevelEncoder, // 小写编码器
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format(timeLayout))
+		},
+		EncodeDuration: zapcore.MillisDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
+	}
+
+	if opt.highlighting {
+		encoderConfig.EncodeLevel = zapcore.LowercaseColorLevelEncoder
+	}
+
+	jsonEncoder := zapcore.NewConsoleEncoder(encoderConfig)
+
+	// Initialize atomicLevel with the initial logging level
+	atomicLevel = zap.NewAtomicLevelAt(opt.level)
+
+	// lowPriority usd by info\debug\warn
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= atomicLevel.Level() && lvl < zapcore.ErrorLevel
+	})
+
+	// highPriority usd by error\panic\fatal
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= atomicLevel.Level() && lvl >= zapcore.ErrorLevel
+	})
+
+	stdout := zapcore.Lock(os.Stdout) // lock for concurrent safe
+	stderr := zapcore.Lock(os.Stderr) // lock for concurrent safe
+
+	core := zapcore.NewTee()
+
+	if !opt.disableConsole {
+		core = zapcore.NewTee(
+			zapcore.NewCore(jsonEncoder,
+				zapcore.NewMultiWriteSyncer(stdout),
+				lowPriority,
+			),
+			zapcore.NewCore(jsonEncoder,
+				zapcore.NewMultiWriteSyncer(stderr),
+				highPriority,
+			),
+		)
+	}
+
+	if opt.file != nil {
+		core = zapcore.NewTee(core,
+			zapcore.NewCore(jsonEncoder,
+				zapcore.AddSync(opt.file),
+				zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+					return lvl >= atomicLevel.Level()
+				}),
+			),
+		)
+	}
+
+	log := zap.New(core,
+		zap.AddCaller(),
+		zap.ErrorOutput(stderr),
+	)
+
+	for key, value := range opt.fields {
+		log = log.WithOptions(zap.Fields(zapcore.Field{Key: key, Type: zapcore.StringType, String: value}))
+	}
+
+	// 给全局logger对象
+	logger = log
+
+	return log, nil
+}
+
 var _ Meta = (*meta)(nil)
 
 // Meta key-value
--- logger_test.go
+++ logger_test.go
@@ -21,8 +21,6 @@ func TestNewJSONLogger(t *testing.T) {
 		WithDisableConsole(),
 		// 时间格式化
 		WithTimeLayout("2006-01-02 15:04:05"),
-		// 高亮
-		WithEnableHighlighting(),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -48,8 +46,43 @@ func TestNewJSONLogger(t *testing.T) {
 	Error("err occurs", zap.String("key", "err.Error()"))
 }
 
+func TestNewTextLogger(t *testing.T) {
+	logger, err := NewTextLogger(
+		// 日志等级
+		WithDebugLevel(),
+		// 写出的文件
+		//WithFileRotationP("./log/test.log"),
+		// 不在控制台打印
+		//WithDisableConsole(),
+		// 时间格式化
+		WithTimeLayout("2006-01-02 15:04:05"),
+		// 高亮
+		WithEnableHighlighting(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer Sync()
+
+	err = errors.New("pkg error")
+	logger.Error("err occurs", WrapMeta(nil, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
+	logger.Error("err occurs", WrapMeta(err, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
+
+	logger.Info("err occurs", zap.String("key", "value"))
+	logger.Warn("err occurs", zap.Any("key", "value"))
+	logger.Error("err occurs", zap.Int("key", 1))
+	logger.Debug("err occurs", zap.Int("key", 1))
+
+	Debug("err occurs", zap.String("key", "err.Error()"))
+	Info("err occurs", zap.String("key", "err.Error()"))
+	Warn("err occurs", zap.String("key", "err.Error()"))
+	Error("err occurs", zap.String("key", "err.Error()"))
+}
+
 func TestAtomicLeve(t *testing.T) {
 	_, err := NewJSONLogger()
+	defer Sync()
 	assert.NoError(t, err)
 	logger.Info("This is a info message")   // This should now be logged
 	logger.Debug("This is a debug message") // This should now be logged
@@ -78,6 +111,9 @@ func TestAtomicLeve(t *testing.T) {
 
 func TestChangeLevelHandlerFunc(t *testing.T) {
 	logger, err := NewJSONLogger()
+	defer func() {
+		Sync()
+	}()
 	assert.NoError(t, err)
 	logger.Info("This is a info message")   // This should now be logged
 	logger.Debug("This is a debug message") // This should now be logged
